934e04565e62b7580d1501166982e03ddaf27b1c
thomasShelby <101035052+Maxim-Zauysev@users.noreply.github.com>
2023-03-08 14:19:47 +0700

    final 2.5

--- src/main/java/com/example/MyBookShopApp/controllers/AuthorsController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/AuthorsController.java
@@ -1,28 +1,32 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.Author;
 import com.example.MyBookShopApp.data.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
-@RequestMapping("/bookshop")
-public class AuthorController {
+public class AuthorsController {
     private final AuthorService authorService;
+
     @Autowired
-    public AuthorController(AuthorService authorService) {
+    public AuthorsController(AuthorService authorService) {
         this.authorService = authorService;
     }
-
-    @GetMapping("/authors")
-    public String authorPage(Model model) {
-        model.addAttribute("authorsMap" , authorService.getAuthorsMap());
-        return "authors/index";
+    @ModelAttribute("authorsMap")
+    public Map<String, List<Author>> authorsMap()
+    {
+        return authorService.getAuthorsMap();
     }
-    @GetMapping("/authors/slug")
-    public String authorPage(){
-        return "authors/slug";
+    @GetMapping("/authors")
+    public String authorsPage(){
+        return "/authors/index";
     }
 }
