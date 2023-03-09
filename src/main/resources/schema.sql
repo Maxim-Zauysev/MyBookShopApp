3955fa9a3ee4723b143d130a88d07670a253ea42
thomasShelby <101035052+Maxim-Zauysev@users.noreply.github.com>
2023-03-09 11:55:30 +0700

    "test "

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,5 @@
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS authors;
-<<<<<<< HEAD
-
-=======
->>>>>>> 5d04d0c88ff344294e15b99237cce84acea8b056
 
 CREATE TABLE  books(
 id BIGSERIAL PRIMARY KEY,
@@ -14,13 +10,8 @@ price VARCHAR(250) DEFAULT NULL
 );
 
 CREATE TABLE  authors(
-<<<<<<< HEAD
     id BIGSERIAL PRIMARY KEY,
     firstName VARCHAR(250) NOT NULL,
     lastName VARCHAR(250) NOT NULL
-=======
-id INT AUTO_INCREMENT PRIMARY KEY,
-firstName VARCHAR(250) NOT NULL,
-lastName VARCHAR(250) NOT NULL
->>>>>>> 5d04d0c88ff344294e15b99237cce84acea8b056
+
 );
