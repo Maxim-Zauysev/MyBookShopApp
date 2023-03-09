476d5f6897d4b98312ccb5ce318e9ccf47e4ad4b
thomasShelby <101035052+Maxim-Zauysev@users.noreply.github.com>
2023-03-09 17:22:31 +0700

    "4.7 Task 1/ step 1"

--- src/main/java/com/example/MyBookShopApp/controllers/GenresPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/GenresPageController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/bookshop")
 public class GenresPageController {
     @GetMapping("/genres")
     public String genresPage() {
-        return "genres/index";
+        return "/genres/index";
     }
+
 }
