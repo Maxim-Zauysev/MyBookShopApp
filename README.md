06f6572f5177623242434a4c9db8c4054b1b9179
thomasShelby <101035052+Maxim-Zauysev@users.noreply.github.com>
2023-03-20 13:34:32 +0700

    5.6 Миграция базы данных

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MyBookShopApp
+![img.png](img.png)
--- src/main/java/com/example/MyBookShopApp/repository/BookRepository.java
+++ src/main/java/com/example/MyBookShopApp/repository/BookRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book,Long> {
     List<Book> findBookByAuthor_FirstName(String name);
     @Query("from Book")
-    List<Book> customFindAllBooks ();
+    List<Book> customFindAllBooks();
 }
